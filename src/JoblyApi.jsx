938f369b14116d2690976a6ec8f35823032e8205
nayani <yapancs@gmail.com>
2024-12-18 12:27:07 -0500

    Initial Commit

--- src/JoblyApi.jsx
+++ src/JoblyApi.jsx
@@ -2,8 +2,8 @@ import axios from "axios";
 
 console.log("api.js loaded successfully");
 
-// const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
-const BASE_URL = "http://localhost:3001";
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
+// const BASE_URL = "http://localhost:3001";
 
 /** API Class.
  *
